84a78b0f4c69a069d2148f97217b4ecb76c9cbf4
Roman <160484622+Romikon@users.noreply.github.com>
2024-02-20 22:59:15 +0200

    Update сапер.cpp

--- сапер/сапер/сапер.cpp
+++ сапер/сапер/сапер.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <random>
 
 using namespace std;
@@ -56,6 +56,8 @@ void Random(int horizont[5], int vertical[5])
         vertical[i] = 1 + rand() % 10;
     }
 
+    cout << "Mines:" << endl;
+
     for (int i = 0; i < 5; i++)
     {
 
@@ -80,78 +82,86 @@ bool User_choise(int a[10][10], int horizont[5], int vertical[5])
     cout << "Enter second number: ";
     cin >> num2;
 
+    if (num1 < 1 || num2 < 1 || num1 > 10 || num2 > 10)
+    {
+        cout << "Enter valid number" << endl;
+        User_choise(a, horizont, vertical);
+    }
+    else
+    {
+        int counter = 0;
+        int start_h = num1 - 1;
+        int start_v = num2 - 1;
+
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (start_h == horizont[j] && start_v == vertical[j])
+                {
+                    counter++;
+                }
+            }
+            start_v++;
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (start_h == horizont[j] && start_v == vertical[j])
+                {
+                    counter++;
+                }
+            }
+            start_h++;
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (start_h == horizont[j] && start_v == vertical[j])
+                {
+                    counter++;
+                }
+            }
+            start_v--;
+        }
+
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (start_h == horizont[j] && start_v == vertical[j])
+                {
+                    counter++;
+                }
+            }
+            start_h--;
+        }
+
+
+        a[num1 - 1][num2 - 1] = counter;
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (num1 == horizont[i] && num2 == vertical[i])
+            {
+                return false;
+            }
+        }
+
+
+        Show_board(a);
+    }
+
+
     
-
-
     
-    int counter = 0;
-    int start_h = num1 - 1;
-    int start_v = num2 - 1;
-
-
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            if (start_h == horizont[j] && start_v == vertical[j])
-            {
-                counter++;
-            }
-        }
-        start_v++;
-    }
-
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            if (start_h == horizont[j] && start_v == vertical[j])
-            {
-                counter++;
-            }
-        }
-        start_h++;
-    }
-
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            if (start_h == horizont[j] && start_v == vertical[j])
-            {
-                counter++;
-            }
-        }
-        start_v--;
-    }
-
-
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            if (start_h == horizont[j] && start_v == vertical[j])
-            {
-                counter++;
-            }
-        }
-        start_h--;
-    }
-
-
-    a[num1 - 1][num2 - 1] = counter;
-
-
-    for (int i = 0; i < 5; i++)
-    {
-        if (num1 == horizont[i] && num2 == vertical[i])
-        {
-            return false;
-        }
-    }
-
-
-    Show_board(a);
 }
 
 
@@ -174,11 +184,6 @@ int main()
     while (is_lose)
     {
         is_lose = User_choise(a, horizont, vertical);
-        is_lose1 = is_lose;
-        if (is_lose1 == true)
-        {
-            Show_board(a);
-        }
     }
 
     cout << "You lose(";
